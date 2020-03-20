521e250538c80341976eebf1e89e331c0676fda2
PekGyerek <k.mategergo1998@gmail.com>
2020-03-20 13:44:25 +0100

    etlap alap

---
+++ src/pages/etlap.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html lang="HU">
+<head>
+    <meta charset="UTF8">
+    <meta description="Kipa étterem étlap">
+    <link rel="stylesheet" type="text/css" href="../styles/etlap_style.css">
+</head>
+<body>
+<div id="etlapDiv">
+    <table id="etlapTable">
+        <caption>étlap<caption>
+        <thead>
+            <tr>
+                <th>név</th><th colspan="2">jellemzés</th><th>ár</th><th>mennyiség</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td name="foodName">humusz</td> <td colspan="2" name="foodDesc">asd</td> <td name="foodPrice">1000 Ft</td> <td></td><td><input type="button" class="hozzaAd" value="kosárba"></td>
+            </tr>
+            <tr>
+              <td name="foodName">sajtos pizza</td> <td colspan="2" name="foodDesc">dsa</td> <td name="foodPrice">1200 Ft</td> <td></td><td><input type="button" class="hozzaAd" value="kosárba"></td>
+            </tr>
+            <tr>
+              <td name="foodName">falafel tál</td> <td colspan="2" name="foodDesc">placeholder</td name="foodPrice"> <td>800 Ft</td> <td></td><td><input type="button" class="hozzaAd" value="kosárba"></td>
+            </tr>
+            <tr>
+               <td name="foodName">steak</td> <td colspan="2" name="foodDesc">rand</td> <td name="foodPrice">1100 Ft</td> <td></td><td><input type="button" class="hozzaAd" value="kosárba"></td>
+            </tr>
+            <tr>
+               <td name="foodName">roston csirke</td> <td colspan="2" name="foodDesc">rand2</td name="foodPrice"> <td>900 Ft</td> <td></td><td><input type="button" class="hozzaAd" value="kosárba"></td>
+            </tr>
+        <tbody>
+        <tfoot>
+        </tfoot>
+    </table>
+</div>
+<script>
+</script>
+</body>
+</html>
