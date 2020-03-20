c65d32f50d7b9b1fdc8b3b93731a8a3b56dd5f72
PekGyerek <k.mategergo1998@gmail.com>
2020-03-20 17:18:48 +0100

    etlap folytatás

--- src/pages/etlap.php
+++ src/pages/etlap.php
@@ -6,6 +6,19 @@
     <link rel="stylesheet" type="text/css" href="../styles/etlap_style.css">
 </head>
 <body>
+<nav><a href="">főoldal</a> <a href="">étlap</a> <a href="">kosár</a>  <a href="">profil</a> <a href="">kijelentkezés</a></nav>
+<div id="mainDiv">
+<div id="kedvencekDiv">
+    <table id="kedvencekTable">
+        <caption>kedvencek</caption>
+        <thead>
+            <th>név</th><th>ár</th><th>mennyiség</th><th></th>
+        </thead>
+        <tbody>
+            <td name="foodName">humusz</td><td name="foodPrice">1000 Ft</td> <td name="foodCount"> <button>-</button> <label for foodCount>0</label> <button>+</button></td><td><input type="button" class="hozzaAd" value="kosárba"></td>
+        </tbody>
+    </table>
+</div>
 <div id="etlapDiv">
     <table id="etlapTable">
         <caption>étlap<caption>
@@ -16,24 +29,25 @@
         </thead>
         <tbody>
             <tr>
-            <td name="foodName">humusz</td> <td colspan="2" name="foodDesc">asd</td> <td name="foodPrice">1000 Ft</td> <td></td><td><input type="button" class="hozzaAd" value="kosárba"></td>
+            <td name="foodName">humusz</td> <td colspan="2" name="foodDesc">asd</td> <td name="foodPrice">1000 Ft</td> <td name="foodCount"> <button>-</button> <label for foodCount>0</label> <button>+</button></td><td><input type="button" class="hozzaAd" value="kosárba"></td>
             </tr>
             <tr>
-              <td name="foodName">sajtos pizza</td> <td colspan="2" name="foodDesc">dsa</td> <td name="foodPrice">1200 Ft</td> <td></td><td><input type="button" class="hozzaAd" value="kosárba"></td>
+              <td name="foodName">sajtos pizza</td> <td colspan="2" name="foodDesc">dsa</td> <td name="foodPrice">1200 Ft</td> <td name="foodCount"> <button>-</button> <label for foodCount>0</label> <button>+</button>  </td><td><input type="button" class="hozzaAd" value="kosárba"></td>
             </tr>
             <tr>
-              <td name="foodName">falafel tál</td> <td colspan="2" name="foodDesc">placeholder</td name="foodPrice"> <td>800 Ft</td> <td></td><td><input type="button" class="hozzaAd" value="kosárba"></td>
+              <td name="foodName">falafel tál</td> <td colspan="2" name="foodDesc">placeholder</td name="foodPrice"> <td>800 Ft</td> <td name="foodCount"> <button>-</button> <label for foodCount>0</label> <button>+</button> </td><td><input type="button" class="hozzaAd" value="kosárba"></td>
             </tr>
             <tr>
-               <td name="foodName">steak</td> <td colspan="2" name="foodDesc">rand</td> <td name="foodPrice">1100 Ft</td> <td></td><td><input type="button" class="hozzaAd" value="kosárba"></td>
+               <td name="foodName">steak</td> <td colspan="2" name="foodDesc">rand</td> <td name="foodPrice">1100 Ft</td> <td name="foodCount"> <button>-</button> <label for foodCount>0</label> <button>+</button> </td><td><input type="button" class="hozzaAd" value="kosárba"></td>
             </tr>
             <tr>
-               <td name="foodName">roston csirke</td> <td colspan="2" name="foodDesc">rand2</td name="foodPrice"> <td>900 Ft</td> <td></td><td><input type="button" class="hozzaAd" value="kosárba"></td>
+               <td name="foodName">roston csirke</td> <td colspan="2" name="foodDesc">rand2</td name="foodPrice"> <td>900 Ft</td> <td name="foodCount"> <button>-</button> <label for foodCount>0</label> <button>+</button> </td><td><input type="button" class="hozzaAd" value="kosárba"></td>
             </tr>
         <tbody>
         <tfoot>
         </tfoot>
     </table>
+</div>
 </div>
 <script>
 </script>
