91239bab5ec0492a765a1396b521349f6392ae74
kopimarci <kopanecz99@gmail.com>
2020-03-30 15:37:52 +0200

    index folytatas

--- src/pages/index.php
+++ src/pages/index.php
@@ -1,12 +1,18 @@
 <!DOCTYPE HTML>
-<html lang="HU">
+<html lang="hu">
 <head>
-
+    <title>Nyitó oldal</title>
+    <meta charset="UTF-8">
+    <meta desciption="KiPa étterem kezdőlap">
 </head>
 <body>
-<div>
-
-<div/>
-<p> Kopanecz Márton és Kiss Máté projektje </p>
+    <header>Kezdőlap</header>
+    <nav>
+        <a href="login.php">bejelentkezés</a>
+        <a href="regisztracio.php">regisztráció</a>
+    </nav>
+    <footer>
+    <h2>Kopanecz Márton és Kiss Máté honlapja</h2>
+    </footer>
 </body>
 </html>
