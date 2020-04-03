250f43b9684f9eb22baac7a1178fc60dffacc76b
kopimarci <kopanecz99@gmail.com>
2020-04-03 17:46:24 +0200

    regisztracio CSS

--- src/pages/regisztracio.php
+++ src/pages/regisztracio.php
@@ -1,20 +1,24 @@
 <!DOCTYPE HTML>
 <html lang="hu">
 <head>
+  <title>Regisztráció</title>
   <meta charset="UTF8">
   <meta description="Regisztrácios oldal">
+   <link rel="stylesheet" type="text/css" href="../styles/regisztracio_style.css">
 </head>
 <body>
-<a href="index.php">Vissza</a> a kezdőlapra
+
   <div class="content">
         <form action="regisztracio.php" method="post">
         <h2>Regisztráció</h2>
                 <label>Felhasználónév:
-                <input type="text" name="username"></label><br>
-                <label>Jelszó:
-                <input type="password" name="password"></label><br>
+                <input type="text" name="username" placeholder="Felhasználónév"></label><br>
+                <label>Jelszó:<br>
+                <input type="password" name="password" placeholder="jelszó"></label<br>
                 <input type="submit" value="Regisztracio">
-            </form>
-        </div>
+                <p><a href="index.php">Vissza</a> a bejelentkezéshez</p>
+        </form>
+   </div>
+   <h5>Kopanecz Márton és Kiss Máté honlapja</h5>
 </body>
 </html>
