901e10f9f0ff79f67bc8dd3ffab3def0f8c1a9b7
PekGyerek <k.mategergo1998@gmail.com>
2020-04-03 19:52:42 +0200

    lot of stuff

--- src/pages/kosar.php
+++ src/pages/kosar.php
@@ -6,4 +6,29 @@
     <link rel="stylesheet" type="text/css" href="../styles/kosar_style.css">
 </head>
 <body>
-<nav><a href="">főoldal</a> <a href="">étlap</a> <a href="">kosár</a>  <a href="">profil</a> <a href="">kijelentkezés</a></nav>
+<div id="mainDiv">
+    <nav><a href="etlap.php">étlap</a> <a href="kosar.php">kosár</a> <a
+                href="profil.php">profil</a>
+        <a href="../script/php/logout.php">kijelentkezés</a>
+    </nav>
+    <form id="orderForm" action="../script/php/makeOrder.php" method="post">
+        <div id="etelekDiv">
+            <p>ételek</p>
+            <div id="etelekKosarban">
+                <p>bulgur</p><p>1000 Ft</p>
+                <p>ez</p><p>1000 Ft</p>
+                <p>az</p><p>1000 Ft</p>
+            </div>
+        </div>
+        <div id="adatokDiv">
+            <p>szállítási adatok</p>
+            <div id="rendeloAdatai">
+                <label>cím:</label><input type="text" name="cim" required="required">
+                <label>telefon:</label><input type="tel" name="telefon" required="required">
+                <label>név:</label><input type="text" name="nev" required="required">
+                <label>megjegyzés:</label><textarea form="orderForm" name=""></textarea>
+                <input type="submit" id="submitButton" value="megrendel">
+            </div>
+        </div>
+    </form>
+</div>
