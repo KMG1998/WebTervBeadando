8b999db3f874c2ca3caff1aeadd92669d0e73542
PekGyerek <k.mategergo1998@gmail.com>
2020-04-05 06:07:09 +0200

    more stuff

--- src/script/JS/profil_JS.js
+++ src/script/JS/profil_JS.js
@@ -1,7 +1,24 @@
 function showEditKedvencekModal() {
-    document.getElementById("editKedvencekModal").style.display ="block";
+    document.getElementById("editModal").style.display ="block";
+    document.getElementById("editKedvencekForm").style.display = "flex";
+    document.getElementById("jelszoCsereForm").style.display = "none";
+    document.getElementById("editAdatokForm").style.display = "none";
 }
 
-function closeEditKedvencekModal() {
-    document.getElementById("editKedvencekModal").style.display ="none";
+function showEditAdatokModal() {
+    document.getElementById("editModal").style.display ="block";
+    document.getElementById("editAdatokForm").style.display = "block";
+    document.getElementById("editKedvencekForm").style.display = "none";
+    document.getElementById("jelszoCsereForm").style.display = "none";
+}
+
+function showJelszoCsereModal() {
+    document.getElementById("editModal").style.display ="block";
+    document.getElementById("jelszoCsereForm").style.display = "block";
+    document.getElementById("editAdatokForm").style.display = "none";
+    document.getElementById("editKedvencekForm").style.display = "none";
+}
+
+function closeEditModal() {
+    document.getElementById("editModal").style.display ="none";
 }
